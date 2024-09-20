ae1d78827c5ae6d8e41c544008f210cc5347581d
Bachagha Mousaab <Mossbachagha@gmail.com>
2024-09-20 23:56:15 +0100

    fix: splashscreen sound interupt external sounds

--- app/src/main/java/com/proglobby/lightsout/SplashScreenActivity.kt
+++ app/src/main/java/com/proglobby/lightsout/SplashScreenActivity.kt
@@ -1,14 +1,19 @@
 package com.proglobby.lightsout
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.widget.VideoView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.security.AccessController.getContext
+
 
 class SplashScreenActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -16,6 +21,7 @@ class SplashScreenActivity : AppCompatActivity() {
         windowManager.defaultDisplay.getMetrics(metrics)
 
         val videoView: VideoView = findViewById<VideoView>(R.id.videoView)
+        videoView.setAudioFocusRequest(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK)
         videoView.setVideoPath("android.resource://" + packageName + "/" + R.raw.f1proglobby)
         val params = videoView.layoutParams
         params.width = metrics.widthPixels
@@ -30,4 +36,5 @@ class SplashScreenActivity : AppCompatActivity() {
             finish()
         }
     }
+
 }
